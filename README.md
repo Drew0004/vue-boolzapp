ff65cb4fbbac5f6db28f401ace25443a67270394
Drew0004 <drdrewgraphic@gmail.com>
2024-01-11 14:57:14 +0100

    Add js instance and body background

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # template-html-css-js
+Link consegna
+https://docs.google.com/document/d/1n7zAM-rjfdqDIzszFA1F6pSZDL4LADU4w8k1RAl8kzM/edit?usp=sharing
---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+const {createApp} = Vue;
+
+createApp({
+  data() {
+    return {
+			//contenuto
+    }
+  },
+	methods: {
+		funzione(){
+			//funzione
+	}
+	}
+}).mount('#app')
